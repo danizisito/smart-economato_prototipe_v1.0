7d28631d3e8d6db1d9e3d33af876b2ce4cf88c88
Daniel <danielrguezhdez06@gmail.com>
2025-11-12 12:28:59 +0000

    oleee

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,19 +1,20 @@
-import { AuthService } from "../services/authServices";
+import { AuthService } from "../services/authServices.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const form = document.getElementById("login-container");
-
-    form.addEventListener("submit", XXXXX => {
+    const form = document.getElementById("login-formulario");
 
 
+    form.addEventListener("submit", async (event) => {
+
+        event.preventDefault();
         const usuario = document.getElementById("username").value;
         const contraseña = document.getElementById("password").value;
 
         try {
 
-            const user = AuthService.login(usuario, contraseña);
-            window.location.href = './templates/main.html';
+            const user = await AuthService.login(usuario, contraseña);
+            window.location.href = './economato.html';
 
         } catch (error) {
             //Vamos viendo que poner
